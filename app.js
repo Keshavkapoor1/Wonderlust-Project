d64d135ee8e1bb942b3be9d5eb7db47b717138aa
keshav kapoor <keshavkapoor12000@gmail.com>
2025-05-28 22:49:09 +0530

    add

--- app.js
+++ app.js
@@ -61,12 +61,13 @@ store.on("error",()=>{
 app.use(session({ 
      store,
      secret:process.env.SECRET,
-   
     resave:false,
     saveUninitialized:true,
+    cookie:{
     expires:Date.now()+7*24*60*60*1000,
     maxAge:7*24*60*60*1000,
     httpOnly:true,
+    },
 }));
 app.use(flash());
 
